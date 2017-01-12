17e85212f2711a64f8f1ea8cafe1db7c65d6857c
Samuele Mattiuzzo <samumatt@gmail.com>
2017-01-12 14:09:24 +0000

    Initial

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,14 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var currentLabel: UILabel!
+    
+    var currentGuess: Int = 0
+    @IBOutlet weak var guessLabel: UILabel!
+    
+    var currentScore: Int = 0
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var currentRound: Int = 0
+    @IBOutlet weak var roundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startNewRound()
+        currentGuess = lroundf(slider.value * 100)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func _randomizeValue() {
+        currentValue = 1 + Int(arc4random_uniform(100))
+    }
+    
+    func startNewRound() {
+        _randomizeValue()
+        slider.value = Float(0.5)
+    }
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is now \(currentGuess), the target value is \(currentValue)"
+        
+        let alert = UIAlertController(title: "Bull's Eye", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved() {
+        currentGuess = lroundf(slider.value * 100)
     }
 
 
