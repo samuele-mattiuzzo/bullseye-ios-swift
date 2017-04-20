ab561c6cd655c24505b7487b589d4ff9f213f0d6
Samuele Mattiuzzo <samumatt@gmail.com>
2017-04-20 22:49:01 +0100

    Test test test

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -24,6 +24,8 @@ class BullsEyeTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true);
+
     }
     
     func testPerformanceExample() {
