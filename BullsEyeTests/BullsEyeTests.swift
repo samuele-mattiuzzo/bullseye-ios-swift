aeca061bdb15d08af2c3dcbf2074f96e46f2e5b2
Samuele Mattiuzzo <samumatt@gmail.com>
2017-04-21 00:13:17 +0100

    Testing Travis and Jenkins at the same time (1)

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -15,6 +15,7 @@ class BullsEyeTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Change
         XCTAssert(true);
 
     }
